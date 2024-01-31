6c54c7b68beb2931533594494a4e4a40adf0209a
c.klewar <c.klewar@f5.com>
2024-01-31 11:21:47 +0100

    0.11.30 v5

--- versions.tf
+++ versions.tf
@@ -8,17 +8,21 @@ terraform {
       name = "f5-xc-aws-ce-module"
     }
   }
-  
+
   required_providers {
+    restapi = {
+      source  = "Mastercard/restapi"
+      version = ">= 1.18.0"
+    }
     volterra = {
-      source = "volterraedge/volterra"
+      source  = "volterraedge/volterra"
       version = "= 0.11.30"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 4.51.0"
     }
     local = ">= 2.2.3"
-    null = ">= 3.1.1"
+    null  = ">= 3.1.1"
   }
 }
